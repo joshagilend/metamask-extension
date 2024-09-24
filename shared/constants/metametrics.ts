2fc0e68498d8e98b73c770ac28ae1ec6afa81b44
IF <139582705+infiniteflower@users.noreply.github.com>
2024-09-24 14:44:59 -0400

    feat: upsert dest chain if user does not have it already, fix manually adding native gas token

--- shared/constants/metametrics.ts
+++ shared/constants/metametrics.ts
@@ -926,6 +926,7 @@ export enum MetaMetricsNetworkEventSource {
   Dapp = 'dapp',
   DeprecatedNetworkModal = 'deprecated_network_modal',
   NewAddNetworkFlow = 'new_add_network_flow',
+  Bridge = 'bridge',
 }
 
 export enum MetaMetricsSwapsEventSource {
