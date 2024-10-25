7ea2b7bba8605ee828801398c8aec953f5d6839d
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-10-25 13:43:08 -0700

    chore: display quote metadata

--- ui/pages/bridge/prepare/bridge-cta-button.tsx
+++ ui/pages/bridge/prepare/bridge-cta-button.tsx
@@ -5,7 +5,6 @@ import {
   getFromAmount,
   getFromChain,
   getFromToken,
-  getToAmount,
   getToChain,
   getToToken,
   getBridgeQuotes,
@@ -21,18 +20,11 @@ export const BridgeCTAButton = () => {
   const toChain = useSelector(getToChain);
 
   const fromAmount = useSelector(getFromAmount);
-  const toAmount = useSelector(getToAmount);
 
   const { isLoading, activeQuote } = useSelector(getBridgeQuotes);
 
   const isTxSubmittable =
-    fromToken &&
-    toToken &&
-    fromChain &&
-    toChain &&
-    fromAmount &&
-    toAmount &&
-    activeQuote;
+    fromToken && toToken && fromChain && toChain && fromAmount && activeQuote;
 
   const label = useMemo(() => {
     if (isLoading && !isTxSubmittable) {
