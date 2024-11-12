b22c344efdf8286ff7cedb038cf75cc37799d06d
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-11-12 13:36:37 -0800

    fix: replace hardcoded values with variables

--- ui/components/multichain/asset-picker-amount/asset-picker-modal/AssetList.tsx
+++ ui/components/multichain/asset-picker-amount/asset-picker-modal/AssetList.tsx
@@ -88,7 +88,7 @@ export default function AssetList({
             padding={0}
             gap={0}
             margin={0}
-            key={`${token.symbol}-${token.address ?? ''}`}
+            key={`${token.symbol}-${tokenAddress ?? ''}`}
             backgroundColor={
               isSelected
                 ? BackgroundColor.primaryMuted
