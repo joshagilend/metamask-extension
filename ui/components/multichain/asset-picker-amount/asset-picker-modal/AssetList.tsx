3f6bd66b50268663c8443723fdd2c53b1c8e0340
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-11-12 13:36:16 -0800

    fix: localize loading text

--- ui/components/multichain/asset-picker-amount/asset-picker-modal/AssetList.tsx
+++ ui/components/multichain/asset-picker-amount/asset-picker-modal/AssetList.tsx
@@ -22,6 +22,7 @@ import {
 } from '../../../../helpers/constants/design-system';
 import { TokenListItem } from '../..';
 import LoadingScreen from '../../../ui/loading-screen';
+import { useI18nContext } from '../../../../hooks/useI18nContext';
 import AssetComponent from './Asset';
 import { AssetWithDisplayData, ERC20Asset, NativeAsset } from './types';
 
@@ -49,6 +50,7 @@ export default function AssetList({
   network,
   isTokenListLoading = false,
 }: AssetListProps) {
+  const t = useI18nContext();
   const selectedToken = asset?.address;
 
   const nativeCurrency = useSelector(getNativeCurrency);
@@ -72,7 +74,7 @@ export default function AssetList({
     <Box className="tokens-main-view-modal">
       {isTokenListLoading && (
         <LoadingScreen
-          loadingMessage={'Loading token list'}
+          loadingMessage={t('loadingTokenList')}
           showLoadingSpinner
         />
       )}
